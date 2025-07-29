f11004ac9677a32c3df6a85e1984258643b1178c
Abdullah Nasir <200837751+abdullahnasir-ai@users.noreply.github.com>
2025-07-29 15:49:46 +0100

    Previewed the raw dataset
    Took a look at the dataset with .head(), .info(), and .describe()

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,6 +4,12 @@ observations_raw = pd.read_csv('/Users/dully/PycharmProjects/biodiversity_endang
 species_info_raw = pd.read_csv('/Users/dully/PycharmProjects/biodiversity_endangerment/data/species_info.csv')
 
 print(observations_raw.head())
-print(species)
+print(species_info_raw.head())
+
+print(observations_raw.info())
+print(species_info_raw.info())
+
+print(observations_raw.describe())
+print(species_info_raw.describe())
 
 
