41de9e8ebad0f96676c5a70565fe190ba6842b65
Abdullah Nasir <200837751+abdullahnasir-ai@users.noreply.github.com>
2025-07-29 15:37:21 +0100

    Initial commit

---
+++ src/data_loader.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+observations_raw = pd.read_csv('/Users/dully/PycharmProjects/biodiversity_endangerment/data/observations.csv')
+species_info_raw = pd.read_csv('/Users/dully/PycharmProjects/biodiversity_endangerment/data/species_info.csv')
+
+print(observations_raw.head())
+print(species)
+
+
