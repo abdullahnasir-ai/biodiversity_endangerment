3138a343d8f614d288a86eda4725581325a92bf9
Abdullah Nasir <200837751+abdullahnasir-ai@users.noreply.github.com>
2025-07-30 16:18:58 +0100

    Created dataframes for the analysis

--- src/analysis.py
+++ src/analysis.py
@@ -2,6 +2,9 @@ from src.clean_data import species_observations
 import matplotlib.pyplot as plt
 
 print('Species by Conservation status:')
+table1 = species_observations['conservation_status'].value_counts()
+table1_df = table1.reset_index()
+table1_df.columns = ['conservation_status', 'count']
 print(species_observations['conservation_status'].value_counts())
 
 percentage_of_endangered = (len(species_observations[species_observations['conservation_status'] == 'Endangered']) / len(species_observations)) * 100
@@ -10,6 +13,9 @@ print(f'Percentage of Endangered Observations: {percentage_of_endangered:.2f}%')
 
 print('Species by Category:')
 print(species_observations['category'].value_counts())
+table2 = species_observations['category'].value_counts()
+table2_df = table2.reset_index()
+table2_df.columns = ['category', 'count']
 
 percentage_of_vascular_plant = (len(species_observations[species_observations['category'] == 'Vascular Plant']) / len(species_observations)) * 100
 print(f'Percentage of Observations that are Vascular Plants: {percentage_of_vascular_plant:.2f}%')
