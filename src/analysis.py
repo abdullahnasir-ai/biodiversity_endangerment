ec113bc69fc28cf2ba65bbb9115d14daff36b111
Abdullah Nasir <200837751+abdullahnasir-ai@users.noreply.github.com>
2025-07-30 14:42:25 +0100

    Analysed the Percentage of Endangered observations

--- src/analysis.py
+++ src/analysis.py
@@ -4,4 +4,8 @@ import matplotlib.pyplot as plt
 print('Species by Conservation status:')
 print(species_observations['conservation_status'].value_counts())
 
+percentage_of_endangered = (len(species_observations[species_observations['conservation_status'] == 'Endangered']) / len(species_observations)) * 100
+print(f'Percentage of Endangered Observations: {percentage_of_endangered:.2f}%')
+# Endangered Observations account for 0.31% of the dataset
+
 
