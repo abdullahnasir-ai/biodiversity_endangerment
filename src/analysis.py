8a4f83f05ee46b543a96027d00e5da50e611ff84
Abdullah Nasir <200837751+abdullahnasir-ai@users.noreply.github.com>
2025-07-30 14:42:45 +0100

    Analysed the number of endangered by species

--- src/analysis.py
+++ src/analysis.py
@@ -8,4 +8,10 @@ percentage_of_endangered = (len(species_observations[species_observations['conse
 print(f'Percentage of Endangered Observations: {percentage_of_endangered:.2f}%')
 # Endangered Observations account for 0.31% of the dataset
 
+print('Species by Category:')
+print(species_observations['category'].value_counts())
+
+percentage_of_vascular_plant = (len(species_observations[species_observations['category'] == 'Vascular Plant']) / len(species_observations)) * 100
+print(f'Percentage of Observations that are Vascular Plants: {percentage_of_vascular_plant:.2f}%')
+# Vascular Plants make up 76.31% of the dataset
 
